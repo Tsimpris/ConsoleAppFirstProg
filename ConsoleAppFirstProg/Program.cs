2f921b86f26ee8a11db52f9d150290dfe25cb4ed
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-09 19:43:50 +0200

    Added a message printing my name

--- ConsoleAppFirstProg/Program.cs
+++ ConsoleAppFirstProg/Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test for first repository!");
 
+Console.WriteLine("Today is 9/1/2023");
+string name = "John";
+Console.WriteLine(name);
 Console.ReadLine();
